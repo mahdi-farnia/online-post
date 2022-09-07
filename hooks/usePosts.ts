0f0e3264cc2bdeaa5e76e94df082b630a2d6073b
Mahdi Farnia <mohammadmahdi1383@outlook.com>
2022-09-07 14:00:07 +0430

    styling

---
+++ hooks/usePosts.ts
@@ -0,0 +1,23 @@
+import useSWR from 'swr';
+import type { Post as IPost } from '@prisma/client';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+interface PostHook {
+  isLoaded: boolean;
+  error?: any;
+  posts?: IPost[] | undefined;
+}
+
+export default function usePosts(): PostHook {
+  const { data, error } = useSWR<IPost[]>('/api/posts', fetcher, {
+    errorRetryCount: 3,
+    errorRetryInterval: 3_000
+  });
+
+  if (error) return { isLoaded: !1, error, posts: [] };
+
+  if (!data) return { isLoaded: !1 };
+
+  return { posts: data, isLoaded: !0 };
+}
